b5698e5b85b8250f8772f03957bf214e19aa3d7e
Om Panigrahi <ompani6167@gmail.com>
2024-12-26 14:32:00 +0530

    Code Redability Enhanced

--- create.js
+++ create.js
@@ -6,12 +6,12 @@ export const main = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
         Item: {
-            // Theattributesoftheitemto be created
-            userId: event.requestContext.identity.cognitoIdentityId, //The id of the author
-            noteId: uuid.v1(), //A unique uuid
-            content: data.content, //Parsed from request body
-            attachment: data.attachment,//Parsed from request body
-            createdAt: Date.now(), //Current Unix time stamp
+            // The attributes of the item to be created
+            userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+            noteId: uuid.v1(), // A unique uuid
+            content: data.content, // Parsed from request body
+            attachment: data.attachment, // Parsed from request body
+            createdAt: Date.now(), // Current Unix timestamp
         },
     };
     await dynamoDb.put(params);
--- update.js
+++ update.js
@@ -1,24 +1,24 @@
-import handler from"./libs/handler-lib";
-import dynamoDb from"./libs/dynamodb-lib";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 export const main = handler(async (event, context) => {
     const data = JSON.parse(event.body);
     const params = {
         TableName: process.env.tableName,
-        //'Key'definesthepartition key andsortkeyoftheitemtobeupdated
+        // 'Key' defines the partition key and sort key of the item to be updated
         Key: {
-            userId: event.requestContext.identity.cognitoIdentityId, //Theidof the author
-            noteId: event.pathParameters.id, //Theidofthenotefromthepath
+            userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+            noteId: event.pathParameters.id, // The id of the note from the path
         },
-        //'UpdateExpression'definestheattributestobeupdated
-        //'ExpressionAttributeValues' definesthevalueintheupdateexpression
-        UpdateExpression: "SET content= :content , attachment=:attachment",
+        // 'UpdateExpression' defines the attributes to be updated
+        // 'ExpressionAttributeValues' defines the value in the update expression
+        UpdateExpression: "SET content = :content, attachment = :attachment",
         ExpressionAttributeValues: {
             ":attachment": data.attachment || null,
             ":content": data.content || null,
         },
-        //'ReturnValues'specifies if and how toreturntheitem'sattributes,
-        //whereALL_NEWreturnsall attributesoftheitemaftertheupdate;you
-        //caninspect'result'belowto seehowitworkswithdifferentsettings
+        // 'ReturnValues' specifies if and how to return the item's attributes,
+        // where ALL_NEW returns all attributes of the item after the update; you
+        // can inspect 'result' below to see how it works with different settings
         ReturnValues: "ALL_NEW",
     };
     await dynamoDb.update(params);
